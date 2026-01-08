8139a70dd1cfee742cf26ecb6d04c6859e5128b4
vinicio281 <vinicio15789@gmail.com>
2026-01-08 20:41:45 -0300

    Atividade prática01 - questão 04

---
+++ atividadepratica01/questao04.py
@@ -0,0 +1,10 @@
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+valor_final = preco_unitario * quantidade
+
+print("Total Compra")
+print(f"produto: {nome_produto}")
+print(f"preço unitário: {preco_unitario:.2f}")
+print(f"Total: {valor_final:.2f}")
